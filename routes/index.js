658b319e7ea4570caa186ff01e01bae2f2c1a930
Rose M <97209503+Rosendo-Martinez@users.noreply.github.com>
2024-04-02 18:20:45 -0500

    Styling/Layout

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/inventory');
+  // res.redirect('/inventory');
+  res.render('index.pug');
 });
 
 module.exports = router;
