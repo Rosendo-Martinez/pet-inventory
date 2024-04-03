2cb8a6cacf30dfa1ec39319ab3e072267fa3a884
Rose M <97209503+Rosendo-Martinez@users.noreply.github.com>
2024-04-03 17:02:22 -0500

    Pet Update Post

--- controller/PetController.js
+++ controller/PetController.js
@@ -121,9 +121,59 @@ exports.pet_update_get = asyncHandler(async (req, res, next) => {
     });
 });
 
-exports.pet_update_post = asyncHandler(async (req, res, next) => {
-    res.send(`Pet update post not implemented. id: ${req.params.id}`);
-});
+exports.pet_update_post = [
+    body('name')
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage("Name must not be empty.")
+        .matches(/^[A-Za-z\s]+$/)
+        .withMessage('Name must be alphabetic.'),
+    body('type')
+        .isIn(['dog', 'cat'])
+        .withMessage('Type must be dog or cat.'),
+    body('date_of_admittance')
+        .isISO8601()
+        .toDate(),
+    body('summary')
+        .trim()
+        .isLength({ min: 1})
+        .escape()
+        .withMessage("Summary must not be empty."),
+    body('age')
+        .isIn(['young', 'mature', 'old'])
+        .withMessage('Age must be young, mature, or old.'),
+    body('domesticated')
+        .isBoolean()
+        .withMessage('Must be true or false.'),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        const pet = new Pet({
+            name: req.body.name,
+            type: req.body.type,
+            date_of_admittance: req.body.date_of_admittance,
+            summary: req.body.summary,
+            age: req.body.age,
+            domesticated: req.body.domesticated,
+            _id: req.params.id
+        });
+
+        if (!errors.isEmpty()) {
+            res.render("pet_create", {
+                title: "Update Pet",
+                pet: pet,
+                error_list: errors.array(),
+            });
+            return;
+        } else {
+            const updatedPet = await Pet.findByIdAndUpdate(req.params.id, pet);
+
+            res.redirect(updatedPet.url);
+        }
+    }
+)];
 
 exports.pet_delete_get = asyncHandler(async (req, res, next) => {
     res.send(`Cat delete get not implemented. id: ${req.params.id}`);
