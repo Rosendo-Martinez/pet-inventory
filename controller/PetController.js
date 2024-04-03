9e7e2464a1532c7e3224ea27c8b5398bd5e6e5ee
Rose M <97209503+Rosendo-Martinez@users.noreply.github.com>
2024-04-03 16:39:07 -0500

    Pet Update Get

--- controller/PetController.js
+++ controller/PetController.js
@@ -106,7 +106,19 @@ exports.pet_details_get = asyncHandler(async (req, res, next) => {
 });
 
 exports.pet_update_get = asyncHandler(async (req, res, next) => {
-    res.send(`Pet update get not implemented. id: ${req.params.id}`);
+    const pet = await Pet.findById(req.params.id).exec();
+
+    if (pet === null) {
+        // No results.
+        const err = new Error("Pet not found");
+        err.status = 404;
+        return next(err);
+    };
+
+    res.render('pet_create', {
+        title: "Update Pet",
+        pet: pet
+    });
 });
 
 exports.pet_update_post = asyncHandler(async (req, res, next) => {
