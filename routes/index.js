3aa848b2c264db2f6dcb0753c2ca32b72418df5b
Rose M <97209503+Rosendo-Martinez@users.noreply.github.com>
2024-04-03 17:32:21 -0500

    Redirect Index

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.redirect('/inventory');
-  res.render('index.pug');
+  res.redirect('/inventory');
 });
 
 module.exports = router;
