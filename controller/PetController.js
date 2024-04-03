0bf946beeab8fe400df8e3b088ab15ebc30a4ac1
Rose M <97209503+Rosendo-Martinez@users.noreply.github.com>
2024-04-02 19:45:43 -0500

    Refactor: reduce # of routes

--- controller/PetController.js
+++ controller/PetController.js
@@ -19,58 +19,30 @@ exports.dog_list = asyncHandler(async (req, res, next) => {
     res.send(`Dog list not implemented.`);
 });
 
-exports.cat_create_get = asyncHandler(async (req, res, next) => {
-    res.send(`Cat create get not implemented.`);
+exports.pet_create_get = asyncHandler(async (req, res, next) => {
+    res.send(`Pet create get not implemented.`);
 });
 
-exports.cat_create_post = asyncHandler(async (req, res, next) => {
-    res.send(`Cat create post not implemented.`);
+exports.pet_create_post = asyncHandler(async (req, res, next) => {
+    res.send(`Pet create post not implemented.`);
 });
 
-exports.dog_create_get = asyncHandler(async (req, res, next) => {
-    res.send(`Dog create get not implemented.`);
+exports.pet_details_get = asyncHandler(async (req, res, next) => {
+    res.send(`Pet details get not implemented. id: ${req.params.id}`);
 });
 
-exports.dog_create_post = asyncHandler(async (req, res, next) => {
-    res.send(`Dog create post not implemented.`);
+exports.pet_update_get = asyncHandler(async (req, res, next) => {
+    res.send(`Pet update get not implemented. id: ${req.params.id}`);
 });
 
-exports.cat_details_get = asyncHandler(async (req, res, next) => {
-    res.send(`Cat details get not implemented. id: ${req.params.id}`);
+exports.pet_update_post = asyncHandler(async (req, res, next) => {
+    res.send(`Pet update post not implemented. id: ${req.params.id}`);
 });
 
-exports.dog_details_get = asyncHandler(async (req, res, next) => {
-    res.send(`Dog details get not implemented. id: ${req.params.id}`);
-});
-
-exports.cat_update_get = asyncHandler(async (req, res, next) => {
-    res.send(`Cat update get not implemented. id: ${req.params.id}`);
-});
-
-exports.cat_update_post = asyncHandler(async (req, res, next) => {
-    res.send(`Cat update post not implemented. id: ${req.params.id}`);
-});
-
-exports.dog_update_get = asyncHandler(async (req, res, next) => {
-    res.send(`Dog update get not implemented. id: ${req.params.id}`);
-});
-
-exports.dog_update_post = asyncHandler(async (req, res, next) => {
-    res.send(`Dog update post not implemented. id: ${req.params.id}`);
-});
-
-exports.cat_delete_get = asyncHandler(async (req, res, next) => {
+exports.pet_delete_get = asyncHandler(async (req, res, next) => {
     res.send(`Cat delete get not implemented. id: ${req.params.id}`);
 });
 
-exports.cat_delete_post = asyncHandler(async (req, res, next) => {
+exports.pet_delete_post = asyncHandler(async (req, res, next) => {
     res.send(`Cat delete post not implemented. id: ${req.params.id}`);
-});
-
-exports.dog_delete_get = asyncHandler(async (req, res, next) => {
-    res.send(`Dog delete get not implemented. id: ${req.params.id}`);
-});
-
-exports.dog_delete_post = asyncHandler(async (req, res, next) => {
-    res.send(`Dog delete post not implemented. id: ${req.params.id}`);
 });
--- routes/inventory.js
+++ routes/inventory.js
@@ -9,32 +9,18 @@ router.get('/cats', pets_controller.cat_list);
 
 router.get('/dogs', pets_controller.dog_list);
 
-router.get('/cats/create', pets_controller.cat_create_get);
+router.get('/create', pets_controller.pet_create_get);
 
-router.post('/cats/create', pets_controller.cat_create_post);
+router.post('/create', pets_controller.pet_create_post);
 
-router.get('/dogs/create', pets_controller.dog_create_get);
+router.get('/:id', pets_controller.pet_details_get);
 
-router.post('/dogs/create', pets_controller.dog_create_post);
+router.get('/:id/update', pets_controller.pet_update_get);
 
-router.get('/cats/:id', pets_controller.cat_details_get);
+router.post('/:id/update', pets_controller.pet_update_post);
 
-router.get('/dogs/:id', pets_controller.dog_details_get);
+router.get('/:id/delete', pets_controller.pet_delete_get);
 
-router.get('/cats/:id/update', pets_controller.cat_update_get);
-
-router.post('/cats/:id/update', pets_controller.cat_update_post);
-
-router.get('/dogs/:id/update', pets_controller.dog_update_get);
-
-router.post('/dogs/:id/update', pets_controller.dog_update_post);
-
-router.get('/cats/:id/delete', pets_controller.cat_delete_get);
-
-router.post('/cats/:id/delete', pets_controller.cat_delete_post);
-
-router.get('/dogs/:id/delete', pets_controller.dog_delete_get);
-
-router.post('/dogs/:id/delete', pets_controller.dog_delete_post);
+router.post('/:id/delete', pets_controller.pet_delete_post);
 
 module.exports = router;
